aa9a8bae3107311b84f81f989d8530a82822911f
Mizuki <104298092+mizukitayama@users.noreply.github.com>
2023-08-03 02:08:14 +0900

    Prettier thing

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,18 @@
 import logo from "../images/logo.png";
 import React, { useState } from "react";
-import { Button, Checkbox, Container, Form } from "semantic-ui-react";
-import { ButtonComponent } from "../components/atoms/ButtonComponent";
+import { Container, Form } from "semantic-ui-react";
 import { ButtonComponentWithFunction } from "../components/atoms/ButtonComponentWithFunction";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [isFetching, setIsFetching] = useState(false);
   const [workspaceId, setWorkspaceId] = useState("");
   const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
 
   const onLogin = () => {
-    navigate('/')
+    navigate("/");
   };
 
   const handleWorkspaceIdChange = (event) => {
@@ -30,43 +29,49 @@ export const Login = () => {
 
   return (
     <>
-        <Container text>
-          <div className="m-[48px]">
-            <div className="border border-gray rounded-md p-[48px] col-3 bg-white">
-              <img src={logo} className="h-[30px]" />
-							<hr className="border-blue"/>
-              <div className="mt-[16px]">
-                <Form>
-                  <Form.Field>
-                    <label>ワークスペースのID</label>
-                    <input
-                      placeholder="Workspace ID"
-                      value={workspaceId}
-                      onChange={handleWorkspaceIdChange}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <label>ユーザーID</label>
-                    <input
-                      placeholder="Slack User ID"
-                      value={userId}
-                      onChange={handleUserIdChange}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <label>パスワード</label>
-                    <input
-                      placeholder="Password"
-                      value={password}
-                      onChange={handlePasswordChange}
-                    />
-                  </Form.Field>
-                  <ButtonComponentWithFunction value={"Log In"} onClick={onLogin} />
-                </Form>
-              </div>
+      <Container text>
+        <div className="mt-[16px] m-[8px] md:m-[48px]">
+          <div className="border border-gray rounded-md p-[48px] col-3 bg-white">
+            <img src={logo} className="h-[40px]" />
+            <hr className="border-blue" />
+            <div className="mt-[32px]">
+              <Form>
+                <Form.Field>
+                  <label>ワークスペースのID</label>
+                  <input
+                    placeholder="Workspace ID"
+                    value={workspaceId}
+                    onChange={handleWorkspaceIdChange}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>ユーザーID</label>
+                  <input
+                    placeholder="Slack User ID"
+                    value={userId}
+                    onChange={handleUserIdChange}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>パスワード</label>
+                  <input
+                    placeholder="Password"
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                </Form.Field>
+                <div className="flex justify-end mr-[16px]">
+                  <ButtonComponentWithFunction
+                    value={"ログイン"}
+                    onClick={onLogin}
+                  />
+                </div>
+              </Form>
             </div>
           </div>
-        </Container>
+        </div>
+      </Container>
     </>
   );
 };
