742cbe14f06d8442443fbd476962fecca0ac55a0
Mizuki <104298092+mizukitayama@users.noreply.github.com>
2023-08-02 10:24:24 +0900

    added routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "semantic-ui-css/semantic.min.css";
-import "./index.js"
+import "./index.js";
 import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="App">
+      <Router>
+        <div>
+          <hr />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
