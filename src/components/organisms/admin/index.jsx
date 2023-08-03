19c9f7e05a09548e4cfb38b264a448ed866dd615
Mizuki <104298092+mizukitayama@users.noreply.github.com>
2023-08-03 09:08:07 +0900

    Added modal to admin page

--- src/components/organisms/admin/index.jsx
+++ src/components/organisms/admin/index.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { ButtonComponentWithFunction } from "../../atoms/ButtonComponentWithFunction";
+import { ButtonComponent } from "../../atoms/ButtonComponent";
 import { CategoryCurrentList } from "../../molecules/Category/currentList";
-import { Container } from "semantic-ui-react";
+import {
+  Container,
+  Modal,
+  Button,
+  Header,
+  Icon,
+  Input,
+} from "semantic-ui-react";
 
 export const Admin = ({ categories }) => {
-  const categories2 = ["adfadf", "adfasdf", "asdfadf", "heh", "qre"];
+  const [isCategoryAddModalOpen, setIsCategoryAddModalOpen] = useState(false);
+  const [isUsersAddModalOpen, setIsUsersAddModalOpen] = useState(false);
+  const [newCategory, setNewCategory] = useState("");
   function addCategory() {
+    setIsCategoryAddModalOpen(false);
+    setNewCategory("");
+    categories.push(newCategory)
     console.log("Todo");
+    console.log(newCategory);
   }
 
   function invitePeople() {
@@ -20,18 +35,45 @@ export const Admin = ({ categories }) => {
           </div>
           <div className="grid gap-[16px]">
             <div className="text-xl font-semibold">カテゴリーの編集</div>
-            <CategoryCurrentList categories={categories2} />
+            <CategoryCurrentList categories={categories} />
             <div className="mx-auto">
-              <ButtonComponentWithFunction
-                value="+ カテゴリーを追加"
-                onClick={addCategory}
-              />
+              <Modal
+                basic
+                onClose={() => setIsCategoryAddModalOpen(false)}
+                onOpen={() => setIsCategoryAddModalOpen(true)}
+                open={isCategoryAddModalOpen}
+                size="small"
+                trigger={
+                  <Button
+                    style={{ backgroundColor: "#005BAB", color: "white" }}
+                  >
+                    カテゴリーを追加
+                  </Button>
+                }
+              >
+                <Modal.Content>
+                  <div className="grid gap-[16px] flex justify-center">
+                    <Input
+                      placeholder="カテゴリー名"
+                      onChange={(event) => setNewCategory(event.target.value)}
+                      className="w-[200px]"
+                      value={newCategory}
+                    />
+                    <ButtonComponentWithFunction
+                      value="+ カテゴリーを追加"
+                      onClick={addCategory}
+                    />
+                  </div>
+                </Modal.Content>
+              </Modal>
             </div>
           </div>
           <div className="grid gap-[16px]">
             <div className="text-xl font-semibold">ユーザー追加</div>
             <div>
-              新しくSlackワークスペースに入った人を一括でGMO 2channelに追加できます。新しいユーザーは追加されてからGMO 2channelをつかえるようになります
+              新しくSlackワークスペースに入った人を一括でGMO
+              2channelに追加できます。新しいユーザーは追加されてからGMO
+              2channelをつかえるようになります
             </div>
             <div className="mx-auto">
               <ButtonComponentWithFunction
