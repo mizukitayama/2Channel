91282af8c0d656fd9be249deb2ac9302939e6878
Mizuki <104298092+mizukitayama@users.noreply.github.com>
2023-08-03 09:08:25 +0900

    Added header link to admin page

--- src/components/organisms/header/index.jsx
+++ src/components/organisms/header/index.jsx
@@ -7,13 +7,19 @@ export const Header = () => {
 	const redirectToHome = () => {
 		navigate('/')
 	}
+  const redirectToAdmin = () => {
+    navigate('/admin')
+  }
   return (
     <>
       <div className="flex flex-row justify-between sticky top-0 z-10 bg-white py-[32px] -mx-[2px]">
         <div className="h-[25px]" onClick={redirectToHome}>
           <Logo />
         </div>
+        <div>
+          <div className="" onClick={redirectToAdmin}>管理者画面</div>
         <ButtonComponent value={"ログアウト"} className="flex justify-end" />
+        </div>
       </div>
     </>
   );
